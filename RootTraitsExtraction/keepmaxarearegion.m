35a57cc07cf5e81576929a358f7cf0bc460d548d
wuqiangithub <zhwuqian6698@163.com>
2023-01-02 21:18:45 +0800

    Delete RootTraitsExtraction directory

--- RootTraitsExtraction/keepmaxarearegion.m
+++
@@ -1,9 +0,0 @@
-function bw=keepmaxarearegion(orgbw)
-imlabel=bwlabel(orgbw);
-stats=regionprops(imlabel,'Area');
-[b,index]=sort([stats.Area],'descend');
-if length(stats)<1
-    bw=imlabel;
-else
-    bw=ismember(imlabel,index(1));
-end
